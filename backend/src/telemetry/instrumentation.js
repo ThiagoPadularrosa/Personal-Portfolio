e06afbf5cfb1aa89c8d4fd1f544364309119f439
TatoCSV <padularrosathiago26@gmail.com>
2026-08-19 20:11:41 -0300

    feat: add OTLPTraceExporter and attributesOnSpan in the http instrumentation
    fix: fix broen bind mount volume of the collector

--- backend/src/telemetry/instrumentation.js
+++ backend/src/telemetry/instrumentation.js
@@ -2,10 +2,8 @@
 import { NodeSDK } from '@opentelemetry/sdk-node';
 import { ConsoleSpanExporter } from '@opentelemetry/sdk-trace-node';
 import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node';
-import {
-  PeriodicExportingMetricReader,
-  ConsoleMetricExporter,
-} from '@opentelemetry/sdk-metrics';
+import { PeriodicExportingMetricReader } from '@opentelemetry/sdk-metrics';
+import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-proto';
 import { resourceFromAttributes } from '@opentelemetry/resources';
 import {
   ATTR_SERVICE_NAME,
@@ -15,25 +13,38 @@ import config from '../config/config.js';
 
 
 const sdk = new NodeSDK({
+  traceExporter: new OTLPTraceExporter,
   resource: resourceFromAttributes({
-    [ATTR_SERVICE_NAME]: config.OTEL_SERVICE_NAME,
     [ATTR_SERVICE_VERSION]: config.SERVICE_VERSION,
   }),
-  traceExporter: new ConsoleSpanExporter(),
   metricReader: new PeriodicExportingMetricReader({
-    exporter: new ConsoleMetricExporter(),
+    exporter: new OTLPTraceExporter,
     // Export metrics every 60seconds
     exportIntervalMillis: 60000,
   }),
   instrumentations: [getNodeAutoInstrumentations({
     // Ensure HTTP instrumentation captures headers for context propagation
+    '@opentelemetry/instrumentation-http': { 
+      enabled: true,
+      applyCustomAttributesOnSpan(span, request) {
+        const routePath = request.route?.path;
+
+        if (!routePath) {
+          return;
+        }
+
+        const route = `${request.baseUrl ?? ""}${routePath}`;
+
+        span.updateName(`${request.method} ${route}`);
+      },
+     },
     '@opentelemetry/instrumentation-express': { enabled: true },
-    '@opentelemetry/instrumentation-http': { enabled: true },
-  })],
+    }),
+  ],
 });
 sdk.start(); // This run opentelemetry SKD before everything else
 
-process.on('SIGTERM', async () => {
+async function shutdown() {
   // Try and catch replaced .then and .catch
   try {
     await sdk.shutdown(); // Shutting down sdk to prevent memory leaks and/or data loss
@@ -43,6 +54,8 @@ process.on('SIGTERM', async () => {
   } finally {
     process.exit(0);
   }
-})
+}
+process.once("SIGTERM", shutdown);
+process.once("SIGINT", shutdown);
 
 export default sdk;
