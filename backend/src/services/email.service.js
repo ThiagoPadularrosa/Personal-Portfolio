f628e86140a011032e8b86192d91727df90b0fb6
TatoCSV <padularrosathiago26@gmail.com>
2026-08-22 20:45:57 -0300

    wip: add trace span to the email service

--- backend/src/services/email.service.js
+++ backend/src/services/email.service.js
@@ -1,57 +1,62 @@
 import nodemailer from 'nodemailer';
 import transporter from '../config/email.config.js';
 import config from '../config/config.js';
+import { trace } from '@opentelemetry/api';
 
 // The execution function to send a message
 export async function sendEmail({ to, subject, text, html }) {
-  try {
-    // This is the message with the email content and headers 
-    const mailOptions = {
-      from: config.EMAIL_FROM,
-      to,
-      subject,
-      text,
-      html,
-    };
+  const tracer = trace.getTracer('portfolio-email.service');
 
-    // The callback. If this is omitted, sendMail return only a Promise.
-    const info = await transporter.sendMail(mailOptions);
-    console.log("Message sent: %s", info.messageId);
+  return tracer.startActiveSpan('send-email', async (span) => {
+    try {
+      // This is the message with the email content and headers 
+      const mailOptions = {
+        from: config.EMAIL_FROM,
+        to,
+        subject,
+        text,
+        html,
+      };
 
-    if (info.rejected.length > 0) {
-      console.warn("Some recipients were rejected by the server:", info.rejected);
-    } else if (!info.accepted || info.accepted.length === 0) {
-      return console.warn("There are no recipients that accepted the message.");
-    }
-    return info;
-  } catch (error) {
-    // This checks for specific Nodemailer error codes
-    switch(error.code) {
-      case "ECONNECTION":
-      case "ETIMEDOUT":
-        console.error("Network issue. Queueing for automatic retry...", error.message);
-        await pushToRetyQueue(mailOptions);
-        break;  
+      // The callback. If this is omitted, sendMail return only a Promise.
+      const info = await transporter.sendMail(mailOptions);
+      console.log("Message sent: %s", info.messageId);
 
-      case "EAUTH":
-        console.error("CRITICAL: SMTP Authentication failed. Alerting internal dev team...", error.message);
-        break;
-      
-      case "EENVELOPE":
-        console.error("Validation error: Invalid addresses.", error.message);
-        console.error("Rejected emails list:", error.message || []);
-        break;
+      if (info.rejected.length > 0) {
+        console.warn("Some recipients were rejected by the server:", info.rejected);
+      } else if (!info.accepted || info.accepted.length === 0) {
+        return console.warn("There are no recipients that accepted the message.");
+      }
+      return info;
+    } catch (error) {
+      // This checks for specific Nodemailer error codes
+      switch(error.code) {
+        case "ECONNECTION":
+        case "ETIMEDOUT":
+          console.error("Network issue. Queueing for automatic retry...", error.message);
+          await pushToRetyQueue(mailOptions);
+          break;  
 
-        default:
-          // The Fall back that runs when the main code (above) fails, to reading raw SMTP response codes if available
-          if (error.responseCode && error.responseCode >= 400 && error.responseCode < 500) {
-            console.warn(`Temporary SMTP Error ${error.responseCode}. Will retry.`);
-            await pushToRetyQueue(mailOptions);
-          } else {
-              console.error(`Fatal SMTP Error ${error.responseCode || 'Unknown'}:`, error.message)
-          }
+        case "EAUTH":
+          console.error("CRITICAL: SMTP Authentication failed. Alerting internal dev team...", error.message);
           break;
+        
+        case "EENVELOPE":
+          console.error("Validation error: Invalid addresses.", error.message);
+          console.error("Rejected emails list:", error.message || []);
+          break;
+
+          default:
+            // The Fall back that runs when the main code (above) fails, to reading raw SMTP response codes if available
+            if (error.responseCode && error.responseCode >= 400 && error.responseCode < 500) {
+              console.warn(`Temporary SMTP Error ${error.responseCode}. Will retry.`);
+              await pushToRetyQueue(mailOptions);
+            } else {
+                console.error(`Fatal SMTP Error ${error.responseCode || 'Unknown'}:`, error.message)
+            }
+            break;
+      }
+      throw error; // This goes forward to my main app controller 
     }
-    throw error; // This goes forward to my main app controller 
-  }
+  });
 }
