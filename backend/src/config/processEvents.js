4f0457c3a2d9205dde478ea4af7feb924f26e214
TatoCSV <padularrosathiago26@gmail.com>
2026-09-05 20:36:53 -0300

    feat: add another shutdown but for the transporter
    fix: fix the mongoose not allow callbacks

--- backend/src/config/processEvents.js
+++ backend/src/config/processEvents.js
@@ -1,28 +1,27 @@
 import mongoose from "mongoose";
+import sdk from "../telemetry/telemetry.mjs";
 import { server } from "../../server.js";
+import transporter from "./email.config.js";
 
 export async function gracefulShutdown(signal) {
   console.log(`Received ${signal}. Closing MongoDB connection, OpenTelemetry SDK, and HTTP server...`);
+  const forceExit = setTimeout(() => {
+    console.error('Forcing shutdown due to timeout');
+    process.exit(1);
+  }, 10000);
   server.close(async () => {
-    console.log('HTTP server closed');
     try {
-    await mongoose.connection.close(false, () => {
-        console.log('MongoDB connection closed');
-        process.exit(0);
-      });
+    await mongoose.connection.close();
     await sdk.shutdown();
-    console.log('MongoDB and OpenTelemetry SDK closed successfully');
-    process.exit(0);   
+    transporter.close();
+    clearTimeout(forceExit);
+    console.log('MongoDB, transporter and OpenTelemetry SDK closed successfully');
+    process.exit(0);    
   } catch (error) { 
     console.error('Error during graceful shutdown:', error);
     process.exit(1);
   }
   });
-
-  setTimeout(() => {
-    console.error('Forcing shutdown due to timeout');
-    process.exit(1);
-  }, 10000);
 };
 
 export function registerGracefulShutdownHandlers() {
