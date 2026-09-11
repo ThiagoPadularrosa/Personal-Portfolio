46744406829a09b331d8d5f757fafe7268e02c05
TatoCSV <padularrosathiago26@gmail.com>
2026-09-11 20:43:18 -0300

    fix: fix middleware error not defined

--- backend/src/middlewares/noResponseHandler.js
+++ backend/src/middlewares/noResponseHandler.js
@@ -1,12 +1,12 @@
-import config from '../config/config.js';
-
 const noResponseHandler = (req, res, next) => {
   const statusCode = res.statusCode !== 200 ? res.statusCode : 404;
 
-  res.status(statusCode).json({
-    message: error.message || 'Not found',
-    stack: config.NODE_ENV === 'production' ? null : error.stack
-  });
+    res.status(statusCode).json({
+      success: false,
+      message: `Route ${req.method} ${req.originalUrl} Not found `,
+    });
+
+  next();
 };
 
 export default noResponseHandler;
